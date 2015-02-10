f6b82efbccfc2f744e0f9a6738469cb72a675f7c
Ankit <ankitgoyal86@gmail.com>
2015-02-10 16:15:01 +0530

    first commit

---
+++ app/src/main/java/com/example/fbreadersampleapp/FBReaderSampleApp.java
@@ -0,0 +1,7 @@
+package com.example.fbreadersampleapp;
+
+import org.geometerplus.zlibrary.ui.android.library.ZLAndroidApplication;
+
+
+public class FBReaderSampleApp extends ZLAndroidApplication {
+}
---
+++ app/src/main/java/com/example/fbreadersampleapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.fbreadersampleapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.geometerplus.android.fbreader.FBReader;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity
+                        implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, FBReader.class);
+        Uri bookUri = Uri.parse("file://"+Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+                .getAbsolutePath()+ File.separator + "sample.epub");
+        Log.d("testing", bookUri.toString());
+        intent.setData(bookUri);
+        intent.setAction(Intent.ACTION_VIEW);
+        startActivity(intent);
+    }
+}
